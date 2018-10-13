0132a00472c0bf6f3b9bd06d2f6205e86e82da15
David Arnold <dar@xoe.solutions>
2018-10-12 20:15:14 -0500

    refactoring: Move makepot from click-odoo-contrib
    Auhtor: Stéphane Bidoul (ACSONE)

--- src/translator.py
+++ src/translator.py
@@ -21,31 +21,113 @@
 #
 
 import logging
+import base64
+import io
+import os
+import re
+import subprocess
 
 import click
 import click_odoo
 
-# from click_odoo import odoo
-
-# from utils import manifest, gitutils
+from utils import manifest, gitutils
 
 _logger = logging.getLogger(__name__)
 
-# --------- INSTRUCTIONS --------------
-# Start building the CLI around main()
-# The click-context is enriched to env,
-# which behaves like an odoo's self.env
-# You might have a look at an example
-# click-odoo pproject to get familiar.
-# Also, don't forget to consult click's
-# excellent docs.
-# -------------------------------------
 
-def main(env):
-    """ Main description of the command.
+LINE_PATTERNS_TO_REMOVE = [
+    r'"POT-Creation-Date:.*?"[\n\r]',
+    r'"PO-Revision-Date:.*?"[\n\r]',
+]
+
+PO_FILE_EXT = '.po'
+POT_FILE_EXT = '.pot'
+
+
+def export_pot(env, module, addons_dir, msgmerge, commit, msgmerge_if_new_pot,
+               commit_message):
+    addon_name = module.name
+    addon_dir = os.path.join(addons_dir, addon_name)
+    i18n_path = os.path.join(addon_dir, 'i18n')
+    pot_filepath = os.path.join(i18n_path, addon_name + POT_FILE_EXT)
+
+    lang_export = env['base.language.export'].create({
+        'lang': '__new__',
+        'format': 'po',
+        'modules': [(6, 0, [module.id])]
+    })
+    lang_export.act_getfile()
+
+    if not os.path.isdir(i18n_path):
+        os.makedirs(i18n_path)
+
+    pot_is_new = not os.path.exists(pot_filepath)
+
+    files_to_commit = set()
+
+    files_to_commit.add(pot_filepath)
+    with io.open(pot_filepath, 'w', encoding='utf-8') as pot_file:
+        file_content = base64.b64decode(lang_export.data).decode('utf-8')
+        for pattern in LINE_PATTERNS_TO_REMOVE:
+            file_content = re.sub(
+                pattern, '', file_content, flags=re.MULTILINE)
+        pot_file.write(file_content)
+
+    if msgmerge or (msgmerge_if_new_pot and pot_is_new):
+        for lang_filename in os.listdir(i18n_path):
+            if not lang_filename.endswith(PO_FILE_EXT):
+                continue
+            lang_filepath = os.path.join(i18n_path, lang_filename)
+            files_to_commit.add(lang_filepath)
+            subprocess.check_call([
+                'msgmerge',
+                '--quiet',
+                '-U',
+                lang_filepath,
+                pot_filepath,
+            ])
+
+    if commit:
+        gitutils.commit_if_needed(
+            list(files_to_commit),
+            commit_message.format(addon_name=addon_name),
+            cwd=addons_dir,
+        )
+
+
+@click.command()
+@click_odoo.env_options(with_rollback=False, default_log_level='error')
+@click.option('--addons-dir', default='.', show_default=True)
+@click.option('--msgmerge / --no-msgmerge', show_default=True,
+              help="Merge .pot changes into all .po files.")
+@click.option('--msgmerge-if-new-pot / --no-msg-merge-if-new-pot',
+              show_default=True,
+              help="Merge .pot changes into all .po files, only if "
+                   "a new .pot file has been created.")
+@click.option('--commit / --no-commit', show_default=True,
+              help="Git commit exported .pot files if needed.")
+@click.option('--commit-message', show_default=True,
+              default="[UPD] Update {addon_name}.pot")
+def main(env, addons_dir, msgmerge, commit, msgmerge_if_new_pot,
+         commit_message):
+    """ Export translation (.pot) files of addons
+    installed in the database and present in addons_dir.
+    Optionally, run msgmerge on the existing .po files to keep
+    them up to date. Commit changes to git, if any.
     """
-    pass
+    addon_names = [
+        addon_name
+        for addon_name, _, _ in manifest.find_addons(addons_dir)
+    ]
+    if addon_names:
+        modules = env['ir.module.module'].search([
+            ('state', '=', 'installed'),
+            ('name', 'in', addon_names),
+        ])
+        for module in modules:
+            export_pot(env, module, addons_dir, msgmerge, commit,
+                       msgmerge_if_new_pot, commit_message)
 
 
-if __name__ == '__main__':  # pragma: no cover
+if __name__ == '__main__':
     main()
--- tests/test_translator.py
+++ tests/test_translator.py
@@ -21,11 +21,138 @@
 #
 
 # import mock
+import os
+import subprocess
 
 import click
+import click_odoo
 from click.testing import CliRunner
 # from click_odoo import odoo
 
 from src.translator import main
 
 # from ..utils import manifest, gitutils
+
+# this extends the addons path of the odoodb and odoocfg fixtures
+test_addons_dir = os.path.join(
+    os.path.dirname(__file__), 'data', 'test_translator')
+
+
+def test_translator_base(odoodb, odoocfg, tmpdir):
+    addon_name = 'addon_test_translator'
+    addon_path = os.path.join(test_addons_dir, addon_name)
+    i18n_path = os.path.join(addon_path, 'i18n')
+    pot_filepath = os.path.join(i18n_path, addon_name + '.pot')
+
+    subprocess.check_call([
+        click_odoo.odoo_bin,
+        '-d', odoodb,
+        '-c', str(odoocfg),
+        '-i', addon_name,
+        '--stop-after-init',
+    ])
+
+    if os.path.exists(pot_filepath):
+        os.remove(pot_filepath)
+
+    result = CliRunner().invoke(main, [
+        '-d', odoodb,
+        '-c', str(odoocfg),
+        '--addons-dir', test_addons_dir,
+    ])
+
+    assert result.exit_code == 0
+    assert os.path.isdir(i18n_path)
+    assert os.path.isfile(pot_filepath)
+    with open(pot_filepath) as f:
+        assert 'myfield' in f.read()
+
+
+def test_translator_msgmerge(odoodb, odoocfg, tmpdir):
+    addon_name = 'addon_test_translator'
+    addon_path = os.path.join(test_addons_dir, addon_name)
+    i18n_path = os.path.join(addon_path, 'i18n')
+    pot_filepath = os.path.join(i18n_path, addon_name + '.pot')
+    fr_filepath = os.path.join(i18n_path, 'fr.po')
+
+    subprocess.check_call([
+        click_odoo.odoo_bin,
+        '-d', odoodb,
+        '-c', str(odoocfg),
+        '-i', addon_name,
+        '--stop-after-init',
+    ])
+
+    if not os.path.exists(i18n_path):
+        os.makedirs(i18n_path)
+    if os.path.exists(pot_filepath):
+        os.remove(pot_filepath)
+    # create empty fr.po, that will be updated by msgmerge
+    with open(fr_filepath, 'w'):
+        pass
+    assert os.path.getsize(fr_filepath) == 0
+
+    result = CliRunner().invoke(main, [
+        '-d', odoodb,
+        '-c', str(odoocfg),
+        '--addons-dir', test_addons_dir,
+        '--msgmerge',
+    ])
+
+    assert result.exit_code == 0
+    assert os.path.getsize(fr_filepath) != 0
+    with open(fr_filepath) as f:
+        assert 'myfield' in f.read()
+
+
+def test_translator_msgmerge_if_new_pot(odoodb, odoocfg, tmpdir):
+    addon_name = 'addon_test_translator'
+    addon_path = os.path.join(test_addons_dir, addon_name)
+    i18n_path = os.path.join(addon_path, 'i18n')
+    pot_filepath = os.path.join(i18n_path, addon_name + '.pot')
+    fr_filepath = os.path.join(i18n_path, 'fr.po')
+
+    subprocess.check_call([
+        click_odoo.odoo_bin,
+        '-d', odoodb,
+        '-c', str(odoocfg),
+        '-i', addon_name,
+        '--stop-after-init',
+    ])
+
+    if not os.path.exists(i18n_path):
+        os.makedirs(i18n_path)
+    if os.path.exists(pot_filepath):
+        os.remove(pot_filepath)
+    # create empty .pot
+    with open(pot_filepath, 'w'):
+        pass
+    # create empty fr.po
+    with open(fr_filepath, 'w'):
+        pass
+    assert os.path.getsize(fr_filepath) == 0
+
+    result = CliRunner().invoke(main, [
+        '-d', odoodb,
+        '-c', str(odoocfg),
+        '--addons-dir', test_addons_dir,
+        '--msgmerge-if-new-pot',
+    ])
+
+    assert result.exit_code == 0
+    # po file not changed because .pot did exist
+    assert os.path.getsize(fr_filepath) == 0
+
+    # now remove pot file so a new one will
+    # be created and msgmerge will run
+    os.remove(pot_filepath)
+
+    result = CliRunner().invoke(main, [
+        '-d', odoodb,
+        '-c', str(odoocfg),
+        '--addons-dir', test_addons_dir,
+        '--msgmerge-if-new-pot',
+    ])
+
+    with open(fr_filepath) as f:
+        assert 'myfield' in f.read()
